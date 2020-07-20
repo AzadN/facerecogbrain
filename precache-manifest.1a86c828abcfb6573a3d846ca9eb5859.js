fb0f25a828020f6567e1cecdc39d60b9b77456ed
Azad Nautiyal <azadnautiyal54.an@gmail.com>
2020-07-21 00:04:22 +0530

    Updates

---
+++ precache-manifest.1a86c828abcfb6573a3d846ca9eb5859.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e5d46cc3b050f0f3778399be4af8c1e",
+    "url": "/facerecogbrain/index.html"
+  },
+  {
+    "revision": "1863b09f9f2649164889",
+    "url": "/facerecogbrain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b8ccea492381bcb3f4f5",
+    "url": "/facerecogbrain/static/css/main.69175b47.chunk.css"
+  },
+  {
+    "revision": "1863b09f9f2649164889",
+    "url": "/facerecogbrain/static/js/2.b2d38b08.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facerecogbrain/static/js/2.b2d38b08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8ccea492381bcb3f4f5",
+    "url": "/facerecogbrain/static/js/main.d2a14ae2.chunk.js"
+  },
+  {
+    "revision": "80ba9183a05bce7b7e20",
+    "url": "/facerecogbrain/static/js/runtime-main.917af022.js"
+  },
+  {
+    "revision": "89bd9705dd6e02ea21bf5c7f5557e4c2",
+    "url": "/facerecogbrain/static/media/brain.89bd9705.png"
+  }
+]);
